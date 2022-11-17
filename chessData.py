3ed9c379941016fe4797ec0abef6c3e240099cf8
Jakub Pawlikowski <pawlikoj@mcmaster.ca>
2022-11-16 20:49:02 -0500

    updated python for saving graphs as pngs

--- chessData.py
+++ chessData.py
@@ -1,5 +1,6 @@
 
 import csv
+from datetime import datetime
 import matplotlib.pyplot as plt
 
 csv_file = './ChessTracker_11052022.csv'
@@ -13,7 +14,7 @@ game_starts = []
 plt.ylabel('Elo')
 plt.title('Rapid 10 Ranking')
 plt.xlabel('Game played')
-plt.invert_yaxis()
+
 
 with open(csv_file) as csvfile:
     spamreader = csv.reader(csvfile)
@@ -28,4 +29,11 @@ for i in range(0, len(elo_befores)):
     print(elo_befores[i] + ', ' + elo_afters[i])
 
 plt.plot(game_starts, elo_befores)
-plt.show()
+plt.gca().invert_yaxis()
+#plt.show()
+
+dt = datetime.now()
+current_time = dt.strftime("%Y%m%d%H%M")
+current_graph_name = str('./graphs/Elo_chart_Rapid-10_' + current_time + '.png')
+
+plt.savefig(current_graph_name, dpi=None, facecolor='w', edgecolor='w', orientation='portrait', format=None, transparent=False, bbox_inches=None, pad_inches=0.1, metadata=None)
